9c718f7451f87d721e530ee717a8a44a8059b41b
Brad Babb <brad_babb@cable.comcast.com>
2026-06-05 16:19:43 -0500

    updates

--- common/strings/case/detect.v
+++ common/strings/case/detect.v
@@ -1,24 +1,22 @@
 module case
 
-
+import strings
 
 pub fn detect(v string) ?Case {
-	if v.contains(" ") {
+	if v.contains(' ') {
 		return Case.phrase
 	}
 
-	if v.contains("_") {
+	if v.contains('_') {
 		return Case.snake
 	}
 
-	if v.contains("-") {
+	if v.contains('-') {
 		return Case.kabab
 	}
 
 	if !v.contains(' ') && !v.contains('-') && !v.contains('_') {
-		
 	}
 
 	return none
 }
-
